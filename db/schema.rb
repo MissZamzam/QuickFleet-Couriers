ee276b273241ab465f0c4ceeabf5b918ff9596bf
Zamzam <Zamzamhassan44@gmail.com>
2022-10-19 17:00:51 +0300

    migrated deliveries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_132526) do
-  create_table "trackings", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "receipt_id"
-    t.string "pick_up"
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_132515) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_134107) do
+  create_table "deliveries", force: :cascade do |t|
+    t.string "image_url"
+    t.string "category"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "receipts", force: :cascade do |t|
     t.string "receipt_no"
     t.string "sender_name"
@@ -24,6 +27,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_132515) do
     t.string "nature_of_goods"
     t.string "pickup"
     t.string "destination"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trackings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "receipt_id"
+    t.string "pick_up"
+    t.string "destination"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
