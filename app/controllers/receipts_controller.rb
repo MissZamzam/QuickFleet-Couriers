b2a15e7cf6c2358c3820b2ff200f8f86257df96c
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-20 11:06:47 +0300

    get all receipts issued

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,2 +1,56 @@
 class ReceiptsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :receipt_not_found
+
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+
+#   skip_before_action :authenticate, only: [:index, :show, :create, :update]
+
+  # GET /receipts
+  def index
+    receipts = Receipt.all
+    render json: receipts
+  end
+
+  #  GET /receipts/:id
+  def show
+    payment = Receipt.find(params[:id])
+    render json: payment, status: :ok
+  end
+
+  # POST/receipts
+  def create
+    payment = Receipt.create!(payment_params)
+    render json: payment, status: :created
+  end
+
+  # PATCH /receipts/:id
+  def update
+    #find
+    payment = Receipt.find(params[:id])
+    Receipt.update!(payment_params)
+    render json: payment, serializer: Tenantreceiptserializer, status: :ok
+  end
+
+  # DELETE /receipts/:id
+  def destroy
+    # find
+    payment = Receipt.find(params[:id])
+    # delete
+    Receipt.destroy
+    head :no_content
+  end
+
+  private
+
+  def payment_params
+    params.permit(:id, :tenant_id, :invoice_no, :amount_paid, :date)
+  end
+
+  def unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def payment_not_found
+    render json: { error: "payment not found" }, status: :not_found
+  end
 end
