702d41ca24c5fddf1dd3a208ecf29674d6222bb7
Zamzam <Zamzamhassan44@gmail.com>
2022-10-21 12:21:17 +0300

    added post request for deliveries

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -11,4 +11,16 @@ class DeliveriesController < ApplicationController
         delivery = Delivery.find(id: params[:id])
         render json: delivery, status: :ok
     end
+
+    def create
+        delivery = Delivery.create!(delivery_params)
+        render json: delivery, status: :created
+    end
+
+    private
+
+    def delivery_params
+        params.permit(:image_url, :category, :description)
+    end
+
 end
