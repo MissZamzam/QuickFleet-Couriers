7e8d60672d0922249805601a19c3a38f10da7ae2
Zamzam <Zamzamhassan44@gmail.com>
2022-10-21 12:29:58 +0300

    added delete request for deliveries

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,27 +1,38 @@
 class DeliveriesController < ApplicationController
 
 
-
+    # GET /rdeliveries
     def index
-        deliveres = Delivery.all 
-        render json: deliveres, status: :ok
+        deliveries = Delivery.all 
+        render json: deliveries
     end
 
+    # GET /deliveries/:id
     def show
         delivery = Delivery.find(id: params[:id])
         render json: delivery, status: :ok
     end
 
+    # POST/deliveries
     def create
         delivery = Delivery.create!(delivery_params)
         render json: delivery, status: :created
     end
 
+    # PATCH /deliveries/:id
     def update
         delivery = Delivery.find(id: params[:id])
         delivery.update!(delivery_params)
         render json: delivery, status: :ok
     end
+
+          # DELETE /deliveries/:id
+    def destroy
+        delivery = Delivery.find(id: params[:id])
+        Delivery.destroy
+        head :no_content
+    end
+
 
     private
 
@@ -29,5 +40,10 @@ class DeliveriesController < ApplicationController
         params.permit(:image_url, :category, :description)
     end
     
+    def unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+      end
+    
+
 
 end
