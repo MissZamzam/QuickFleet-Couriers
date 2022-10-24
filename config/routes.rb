8b041ca01a10ca25ed084e346f5df8fc868b80d3
Zamzam <Zamzamhassan44@gmail.com>
2022-10-24 09:05:42 +0300

    added custom routes to deliveries

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :deliveries
+  resources :deliveries, only: [:index, :show, :create, :update, :destroy]
   resources :trackings
   resources :receipts
   resources :users
