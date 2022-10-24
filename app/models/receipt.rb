f69f036ec910cee07dd11f09b4dc788614b848fe
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-24 19:42:21 +0300

    update seeds data

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,10 +1,10 @@
 class Receipt < ApplicationRecord
     has_one :tracking
     belongs_to :delivery
-    belongs_to :order
+    # belongs_to :order
 
 
-    validates :receipt_no, uniqueness: true
+    # validates :receipt_no, uniqueness: true
 
     validates :sender_name, :receiver_name, :destination,  presence: true
 end
