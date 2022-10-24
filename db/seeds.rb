fb7eea34feb01ea8c9008b96da8106f98fdbec64
Zamzam <Zamzamhassan44@gmail.com>
2022-10-25 00:01:45 +0300

    made changes to service data in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 delivery1 = Delivery.create(
-    image_url: "https://img.freepik.com/free-photo/white-pill-container-blue-background_23-2148506747.jpg?w=2000",
+    image_url: "https://www.kindpng.com/picc/m/115-1153565_fruit-and-vegetable-basket-png-transparent-png.png",
     category: "On-Demand Delivery",
     description: "On-demand delivery simply means that customers have a choice to decide not just where they would like their products to be delivered, but also when — that is within the shortest delivery time possible"
 )
 
 delivery2 = Delivery.create(
-    image_url: "https://target.scene7.com/is/image/Target/GUEST_5dfc3ae5-0771-4e24-884f-494f922db908?wid=488&hei=488&fmt=pjpeg",
+    image_url: "https://media.istockphoto.com/photos/brown-recycled-paper-gift-parcels-picture-id520844431?k=20&m=520844431&s=170667a&w=0&h=rpidmCiVKYQ7V9tpWR42sQnvnv5asOu0MiMJ_muObao=",
     category: "Parcel Services",
     description: "Parcel shipping refers to shipping lighter, smaller boxed items. Typically, parcel means packages that weigh 100 pounds or less and can be moved without assistance. If you need to ship a few small packages at a time, parcel shipping is an economical way to ship your products."
 )
@@ -15,7 +15,7 @@ delivery3 = Delivery.create(
     description: "Overnight shipping is a delivery service that guarantees shipments will be delivered the next day by a certain time."
 )
 delivery4 = Delivery.create(
-    image_url: "https://www.kindpng.com/picc/m/115-1153565_fruit-and-vegetable-basket-png-transparent-png.png",
+    image_url: "https://img.freepik.com/free-photo/white-pill-container-blue-background_23-2148506747.jpg?w=2000",
     category: "Same Day Courier",
     description: "Same-day delivery is the act of delivering a package the same day it was sent. If a package is delivered before noon, for example, it will be delivered the same day, in the afternoon. Companies that offer this service advertise it as a same-day service."
 )
