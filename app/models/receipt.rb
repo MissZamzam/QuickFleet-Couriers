5b3e4c1dd51944aa1b63e73cbae171c6632ec946
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-24 13:09:41 +0300

    validate receipt details

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -2,4 +2,9 @@ class Receipt < ApplicationRecord
     has_one :tracking
     belongs_to :delivery
     belongs_to :order
+
+
+    validates :receipt_no, uniqueness: true
+
+    validates :sender_name, :receiver_name, :destination,  presence: true
 end
