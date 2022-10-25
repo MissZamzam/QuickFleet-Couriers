bbcc235b6010ac6799205c0c528e5db8f4dce6f1
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-25 20:38:12 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_134107) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_081826) do
   create_table "deliveries", force: :cascade do |t|
     t.string "image_url"
     t.string "category"
@@ -35,44 +35,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_134107) do
   create_table "trackings", force: :cascade do |t|
     t.integer "user_id"
     t.integer "receipt_id"
-    t.string "pick_up"
-    t.string "destination"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "email"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trackings", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "receipt_id"
-    t.string "pick_up"
-    t.string "destination"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "email"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trackings", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "receipt_id"
     t.string "pickup"
     t.string "destination"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "use_profiles", force: :cascade do |t|
+    t.string "avatar"
+    t.string "name"
+    t.string "email"
+    t.string "telephone"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
