9033c21d238930b25751491ba0059939c76fa852
Zamzam <Zamzamhassan44@gmail.com>
2022-10-25 11:26:32 +0300

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_134107) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_081826) do
   create_table "deliveries", force: :cascade do |t|
     t.string "image_url"
     t.string "category"
@@ -36,6 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_134107) do
     t.integer "receipt_id"
     t.string "pick_up"
     t.string "destination"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "use_profiles", force: :cascade do |t|
+    t.string "avatar"
+    t.string "name"
+    t.string "email"
+    t.string "telephone"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
