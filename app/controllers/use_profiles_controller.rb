da6c0494975c7cf92953a62f613b904f4cf7e95d
Zamzam <Zamzamhassan44@gmail.com>
2022-10-26 10:37:19 +0300

    added not found or unprocessable entity if user_profile cannot be found

--- app/controllers/use_profiles_controller.rb
+++ app/controllers/use_profiles_controller.rb
@@ -1,44 +1,52 @@
 class UseProfilesController < ApplicationController
-    # GET /userprofiles
-    def index
-        user_profiles = User_Profile.all
-        render json: user_profiles
-    end
+    rescue_from ActiveRecord::RecordNotFound, with: :user_profile_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
 
 
-    # GET /userprofiles/:id
-    def show
-        user_profile = User_Profile.find_by(id: params[:id])
-        render json: user_profile, status: :ok
-    end
+        # GET /userprofiles
+        def index
+            user_profiles = User_Profile.all
+            render json: user_profiles
+        end
 
+        # GET /userprofiles/:id
+        def show
+            user_profile = User_Profile.find_by(id: params[:id])
+            render json: user_profile, status: :ok
+        end
 
-    # POST/userprofile
-    def create
-        user_profile = User_Profile.create!(user_profile_params)
-        render json: user_profile, status: :created
-    end
+        # POST/userprofile
+        def create
+            user_profile = User_Profile.create!(user_profile_params)
+            render json: user_profile, status: :created
+        end
 
- # PATCH /user_profile/:id
- def update
-    user_profile = User_Profile.find(id: params[:id])
-    user_profile.update!(user_profile_params)
-    render json: user_profile, status: :ok
-end
-  
+        # PATCH /user_profile/:id
+        def update
+            user_profile = User_Profile.find(id: params[:id])
+            user_profile.update!(user_profile_params)
+            render json: user_profile, status: :ok
+        end
 
+        # DELETE /user_profiles/:id
+        def destroy
+            user_profile = User_Profile.find(id: params[:id])
+            User_Profile.destroy
+            head :no_content
+        end
 
-    # DELETE /user_profiles/:id
-    def destroy
-        user_profile = User_Profile.find(id: params[:id])
-        User_Profile.destroy
-        head :no_content
-    end
+        private
 
-    private
+        def user_profile_params
+            params.permit(:avatar, :name, :email, :telephone, :location)
+        end
 
-    def user_profile_params
-        params.permit(:avatar, :name, :email, :telephone, :location)
-    end
+        def unprocessable_entity(invalid)
+            render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+        end
+
+        def delivery_not_found
+            render json: { error: "user_profile not found" }, status: :not_found
+        end
 
 end
