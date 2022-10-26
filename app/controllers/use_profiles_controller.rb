8e76586c12d016c73921f3836876ca3e667427ae
Zamzam <Zamzamhassan44@gmail.com>
2022-10-26 10:26:22 +0300

    added get reequest for userprofile

--- app/controllers/use_profiles_controller.rb
+++ app/controllers/use_profiles_controller.rb
@@ -1,2 +1,7 @@
 class UseProfilesController < ApplicationController
+    # GET /rdeliveries
+    def index
+        user_profiles = User_Profile.all
+        render json: user_profiles
+    end
 end
