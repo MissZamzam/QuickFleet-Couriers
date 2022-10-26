75e3c7a872a9bde7c35bc3028faa277d390edcc7
Zamzam <Zamzamhassan44@gmail.com>
2022-10-26 22:37:12 +0300

    migrated and seeded service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,50 +1,50 @@
 class ServicesController < ApplicationController
 
-    rescue_from ActiveRecord::RecordNotFound, with: :service_not_found
-    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+        rescue_from ActiveRecord::RecordNotFound, with: :service_not_found
+        rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
 
 
-    # GET /services
-    def index
-        services = Service.all
-        render json: services
-    end
+        # GET /services
+        def index
+            services = Service.all
+            render json: services
+        end
 
-     # GET /services/:id
-     def show
-        service = Service.find_by(id: params[:id])
+        # GET /services/:id
+        def show
+            service = Service.find_by(id: params[:id])
+            render json: service, status: :ok
+        end
+        # POST/services
+        def create
+            service = Service.create!(service_params)
+            render json: service, status: :created
+        end
+    # PATCH /services/:id
+    def update
+        service = Service.find(id: params[:id])
+        service.update!(service_params)
         render json: service, status: :ok
     end
-    # POST/services
-    def create
-        service = Service.create!(service_params)
-        render json: service, status: :created
-    end
-# PATCH /services/:id
-def update
-    service = Service.find(id: params[:id])
-    service.update!(service_params)
-    render json: service, status: :ok
-end
 
- # DELETE /deliveries/:id
- def destroy
-    service = Service.find(id: params[:id])
-    Service.destroy
-    head :no_content
-end
-
-    private
-
-    def service_params
-        params.permit(:image_url, :category, :description)
+    # DELETE /deliveries/:id
+    def destroy
+        service = Service.find(id: params[:id])
+        Service.destroy
+        head :no_content
     end
 
-    def unprocessable_entity(invalid)
-        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
-    end
+        private
 
-    def delivery_not_found
-        render json: { error: "service not found" }, status: :not_found
-      end
+        def service_params
+            params.permit(:image_url, :category, :description)
+        end
+
+        def unprocessable_entity(invalid)
+            render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+        end
+
+        def delivery_not_found
+            render json: { error: "service not found" }, status: :not_found
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_101129) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_140911) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,6 +65,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_101129) do
     t.string "pickup"
     t.string "destination"
     t.integer "delivery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "image_url"
+    t.string "category"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
