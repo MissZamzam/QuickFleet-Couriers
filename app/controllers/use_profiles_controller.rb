402737c94e11b0ba77e4395302a98b9f92cd6a24
Zamzam <Zamzamhassan44@gmail.com>
2022-10-26 10:27:40 +0300

    added get request by id for userprofile

--- app/controllers/use_profiles_controller.rb
+++ app/controllers/use_profiles_controller.rb
@@ -1,7 +1,14 @@
 class UseProfilesController < ApplicationController
-    # GET /rdeliveries
+    # GET /userprofiles
     def index
         user_profiles = User_Profile.all
         render json: user_profiles
+    end
+
+
+    # GET /userprofiles/:id
+    def show
+        user_profile = User_Profile.find_by(id: params[:id])
+        render json: user_profile, status: :ok
     end
 end
