3f31cfb6cb5b79df80f2ebc239d564d23a45f22a
Newton Ombese <newtonombese1@gmail.com>
2022-10-27 14:32:53 +0300

    took the risk of migrating and I hope all will work well

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_111340) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_111859) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,6 +50,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_111340) do
     t.string "destination"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "senderName"
+    t.string "receiverName"
+    t.integer "amountPaid"
+    t.string "natureOfGoods"
   end
 
   create_table "receipts", force: :cascade do |t|
