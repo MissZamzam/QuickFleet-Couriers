6011dd7b49d6457974ca6167853a7532657be56b
alvo254 <alvinkhamron@gmail.con>
2022-10-30 19:43:34 +0300

    middleware config

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Authy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    # config.middleware.use ActionDispatch::Session::CookieStore
 
     # Configuration for the application, engines, and railties goes here.
     #
