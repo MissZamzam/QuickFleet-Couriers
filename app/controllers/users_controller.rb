e1c9e093f95f33d025296e5250bd9dd926771d0f
alvo254 <alvinkhamron@gmail.con>
2022-10-30 19:39:47 +0300

    remove dead code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,17 +13,7 @@ class UsersController < ApplicationController
        render json: current_user
     end
 
-    # def create
-    #     user = User.create!(user_params)
-    #     if user.valid?
-    #         session[:user_id] = user.id
-    #         render json: user, status: :created
-    #     else
-    #         render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    #     end
-    # end
     
-
     private
 
     def this_user
