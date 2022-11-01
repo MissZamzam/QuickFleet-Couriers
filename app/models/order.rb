41e7577096e6bba6569c25851f9d07e27ba72927
loretta dhahabu <loreydjefwa@gmail.com>
2022-11-01 16:32:50 +0300

    validate receipts

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord  
-    
+    belongs_to :use_profile
+
+    has_one :receipt
+
+    # has_one :tracking
 end
--- app/models/use_profile.rb
+++ app/models/use_profile.rb
@@ -1,3 +1,5 @@
 class UseProfile < ApplicationRecord
     belongs_to :user
+
+    has_many :orders
 end
