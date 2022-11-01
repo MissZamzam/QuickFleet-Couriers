692abdb35817190e393f73e241cd3e5688184f8a
Zamzam <Zamzamhassan44@gmail.com>
2022-11-01 23:14:50 +0300

    added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,12 +34,12 @@ service6 = Service.create(
 service7 = Service.create(
     image_url: "https://media.sciencephoto.com/image/f0220694/800wm",
     category: "Bulk",
-    description: "Same-day delivery is the act of delivering a package the same day it was sent. If a package is delivered before noon, for example, it will be delivered the same day, in the afternoon. Companies that offer this service advertise it as a same-day service."
+    description: "We charge bulk services per CBM ( cubic meters ). We ship goods to county headquarters from where we distribute to other branches for collection."
 )
 service8 = Service.create(
     image_url: "https://i.pinimg.com/236x/ef/4f/42/ef4f429cf7304f0c76a8f7c9e37fe24d.jpg",
     category: "Fragile Goods",
-    description: "Same-day delivery is the act of delivering a package the same day it was sent. If a package is delivered before noon, for example, it will be delivered the same day, in the afternoon. Companies that offer this service advertise it as a same-day service."
+    description: "QuickFleet is a great option to trust for shipping your breakable products. QuickFleet can do the hard work for you by making sure your package is safe and intact. Plus, QuickFleet offers multiple shipping box sizes so they can customize a box fit for your needs."
 )
 
 
