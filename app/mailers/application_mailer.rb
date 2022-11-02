0f68f711ca6931148a6d10b615663095ac3efaf6
loretta dhahabu <loreydjefwa@gmail.com>
2022-11-02 10:32:12 +0300

    generate an email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'loretta.jefwa@student.moringaschool.com'
   layout 'mailer'
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,21 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3004 }
+  
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3004 }
+    config.action_mailer.delivery_method = :smtp
+  host = 'example.com' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => "dhahabu",
+  :password             => "dhahabu123",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
