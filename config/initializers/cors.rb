6c52905a26bc307731f448a12957aff5c589efeb
Zamzam <Zamzamhassan44@gmail.com>
2022-11-05 13:03:55 +0300

    origin to any

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://quick-fleet-couriers-client-cjzysp89f-misszamzam.vercel.app"
+    origins "*"
 
     resource "*",
       headers: :any,
