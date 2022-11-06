cffd2a5919d59aec533c5c37b8317d8682b572ea
Whitneyjoymuchira <whitneymuchira@gmail.com>
2022-11-06 21:35:31 +0300

    user profile maintenance

--- app/controllers/use_profiles_controller.rb
+++ app/controllers/use_profiles_controller.rb
@@ -4,38 +4,45 @@ class UseProfilesController < ApplicationController
 
 
         # GET /userprofiles
-        def index
-            user_profiles = User_Profile.all
-            render json: user_profiles
-        end
+        # def index
+        #     user_profiles = UseProfile.all
+        #     render json: user_profiles
+        # end
 
         # GET /userprofiles/:id
         def show
-            user_profile = User_Profile.find_by(id: params[:id])
+            user_profile = UseProfile.find_by(id: params[:id])
             render json: user_profile, status: :ok
         end
 
         # POST/userprofile
         def create
-            user_profile = User_Profile.create!(user_profile_params)
-            render json: user_profile, status: :created
+            @user_profile = UseProfile.new(user_profile_params)
+            @user_profile.user_id = 10
+            @user_profile.save
+            render json: user_profile, status: :ok
+            # render json: user_profile, status: :created
         end
 
         # PATCH /user_profile/:id
         def update
-            user_profile = User_Profile.find(id: params[:id])
+            user_profile = UseProfile.find(params[:id])
             user_profile.update!(user_profile_params)
             render json: user_profile, status: :ok
         end
 
         # DELETE /user_profiles/:id
         def destroy
-            user_profile = User_Profile.find(id: params[:id])
-            User_Profile.destroy
+            user_profile = UseProfile.find_by(id: params[:id])
+            user_profile.destroy
             head :no_content
         end
 
         private
+
+        # def set_profile
+        #     @profile =UseProfile.find(params[:id])
+        # end
 
         def user_profile_params
             params.permit(:avatar, :name, :email, :telephone, :location)
